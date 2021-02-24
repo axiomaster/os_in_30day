906a412ddc1c4f9e7dab47b64b9ddb42e56f009d
axiomaster <jiangshanliuyun@gmail.com>
2021-02-25 01:21:49 +0800

    clean up

---
+++ 3_kernel/kernel/main.c
@@ -0,0 +1,19 @@
+/*
+ * ==================================================
+ *
+ *       Filename:  bootpack.c
+ *
+ *    Description:  
+ *
+ *        Version:  0.01
+ *        Created:  2014年03月01日 星期六 22时03分34秒
+ *         Author:  ChrisZZ, zchrissirhcz@163.com
+ *        Company:  ZJUT
+ *
+ * ==================================================
+ */
+#include<header.h>
+int vmain(void){
+	while(1);
+	return 0;
+}
